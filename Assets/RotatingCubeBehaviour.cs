e4f49f52186cadd758235cf11c9c2760925b29cc
js6pak <kubastaron@hotmail.com>
2021-12-21 18:53:43 +0100

    Initial

---
+++ Assets/RotatingCubeBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RotatingCubeBehaviour : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(new Vector3(180, 180, 0) * Time.deltaTime);
+    }
+}
